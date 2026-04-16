80eec1a702d22a786edf1c26065e32a1dc83c2f7
ShipluSaha995 <shiplusaha995@gmail.com>
2026-04-16 18:12:54 +0600

    Updated

--- job.py
+++ job.py
@@ -28,8 +28,52 @@ def post_job(emp_id):
 
 
 def view_jobs():
-    pass
+    conn=connect()
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM jobs")
+    for j in cur.fetchall():
+        print(j)
+    conn.close()
+
+
 def search_jobs():
-    pass
+    conn=connect()
+    cur=conn.cursor()
+
+    user_loc=input("Your Location: ")
+    min_sal=int(input("Minimum Salary: "))
+
+    cur.execute("SELECT job_id, title,location,salary FROM jobs WHERE salary>=%s",(min_sal,))
+    jobs=cur.fetchall()
+
+    dist=dijkstra(graph,user_loc)
+    ranked=[]
+    for j in jobs:
+        d=dist.get(j[2],9999)
+        priority=d-(j[3]/1000)
+        ranked.append(j,priority)
+
+    for i in range(len(ranked)):
+        for j in range(i+1, len(ranked)):
+            if ranked[j][1]<ranked[i][1]:
+                ranked[i],ranked[j]=ranked[j],ranked[i]
+
+
+    print("\n Best Jobs: ")
+    for job, p in ranked:
+        print(job, "Priority: ",p)
+
+    conn.close()
+
+
+
 def search_by_catagory():
-    pass
+    conn=connect()
+    cur=conn.cursor()
+    cat=input("Category: ")
+    cur.execute("SELECT * FROM jobs WHERE category=%s",(cat,))
+
+    for j in cur.fetchall():
+        print(j)
+        
+    conn.close()
