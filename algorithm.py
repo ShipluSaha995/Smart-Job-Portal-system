886c20ff7fc0fde87abad9b4ddb3070f3fa97d31
ShipluSaha995 <shiplusaha995@gmail.com>
2026-04-16 16:47:31 +0600

    Updated

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,27 @@
 def calculate_score(req, usr, exp):
-    pass
-def top_k(data, k):
-    pass
+    weigths={"python":10, "sql":8, "c++":6,"java":7}
+
+    req_list=req.lower().split(",")
+    usr_list=usr.lower().split(",")
+
+    score=0
+
+    for skill in req_list:
+        skill=skill.strip()
+        if skill in usr_list:
+            score+=weigths.get(skill, 5)
+
+    
+
+    score+=exp*3
+    return score
+
+
+
+def top_k(data, k=3):
+    for i in range(len(data)):
+        for j in range(i+1, len(data)):
+            if data[j][1]>data[i][1]:
+                data[i],data[j]=data[j],data[i]
+    
+    return data[:k]
--- dijkstra.py
+++ dijkstra.py
@@ -1,2 +1,24 @@
-def dijkstra(graph, start):
-    pass
+def dijkstra(graph,start):
+    dist={n:9999 for n in graph}
+    visited={n:False for n in graph}
+    dist[start]=0
+
+    while True:
+        min_node=None
+        min_val=9999
+
+        for n in graph:
+            if not visited[n] and dist[n]<min_val:
+                min_val=dist[n]
+                min_node=n
+
+        if min_node is None:
+            break
+
+        visited[min_node]=True
+
+        for nei in graph[min_node]:
+            if dist[min_node]+graph[min_node][nei]<dist[nei]:
+                dist[nei]=dist[min_node]+graph[min_node][nei]
+
+    return dist
