c6e6e4ae28adce6f749e0fe3f036f64e97c1cd68
ShipluSaha995 <shiplusaha995@gmail.com>
2026-04-16 17:34:11 +0600

    Updated

--- job.py
+++ job.py
@@ -3,7 +3,30 @@ from graph import graph
 from dijkstra import dijkstra
 
 def post_job(emp_id):
-    pass
+    
+    conn=connect()
+    cur=conn.cursor()
+
+    title=input("Title: ")
+    company=input("Company: ")
+    location=input("Location: ")
+    salary=int(input("Salary: "))
+    skills=input("Skills: ")
+    category=input("Catagory: ")
+
+    cur.execute(
+        """
+        INSERT INTO jobs(title,company,location,salary,required_skills,category,employer_id)
+        VALUES(%s,%s,%s,%s,%s,%s,%s)
+        """, (title, company,location,salary,skills,category,emp_id)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+
+
 def view_jobs():
     pass
 def search_jobs():
