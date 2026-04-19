f64b80f442aa4c7d45b301b74205a2ee599d1499
ShipluSaha995 <shiplusaha995@gmail.com>
2026-04-19 08:15:47 +0600

    Updated

--- application.py
+++ application.py
@@ -2,6 +2,46 @@ from db import connect
 from algorithm import calculate_score, top_k
 
 def apply_job(uid):
-    pass
+    conn=connect()
+    cur=conn.cursor()
+
+    job_id=int(input("Job ID: "))
+    skills=input("Skills: ")
+    exp=int(input("Experience: "))
+
+    cur.execute("SELECT required_skills FROM jobs WHERE job_id=%s",(job_id,))
+    job=cur.fetchone()
+
+    if not job:
+        print("Invalid job.")
+        return 
+    score=calculate_score(job[0],skills,exp)
+
+    try:
+        cur.execute("""
+        INSERT INTO applications(user_id,job_id,skills,experience,score)
+        VALUE(%s,%s,%s,%s,%s)
+        """,(uid,job_id,skills,exp,score))
+
+        conn.commit()
+        print("Applied Successfully.")
+    
+    except:
+        print("Already Applied.")
+    conn.close()
+
+
 def view_applicants(job_id):
-    pass
+    
+    conn=connect()
+    cur=conn.cursor()
+
+    cur.execute("SELECT user_id,score FROM applicantions WHERE job_id=%s",(job_id,))
+    data=cur.fetchall()
+
+    top=top_k(data)
+
+    print("\nTop APplications.\n")
+    for t in top:
+        print(t)
+    conn.close()
