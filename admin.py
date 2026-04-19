803c328f0f3da55f19f4a906b7d7393539f33453
ShipluSaha995 <shiplusaha995@gmail.com>
2026-04-19 09:19:53 +0600

    Updated

--- admin.py
+++ admin.py
@@ -3,17 +3,63 @@ from edges import edges
 from kruskal import kruskal
 
 def view_all_users():
-    pass
+    conn=connect()
+    cur=conn.cursor()
+    cur.execute("SELECT user_id,name,email,role FROM users")
+    for u in cur.fetchall():
+        print(u)
+    conn.close()
 
 def view_all_jobs():
-    pass
+    
+    conn=connect()
+    cur=conn.cursor()
+
+    cur.execute("SELECT * FROM jobs")
+    for j in cur.fetchall():
+        print(j)
+    conn.close()
 
 def view_all_applications():
-    pass
+    conn=connect()
+    cur=conn.cursor()
+
+    cur.execute("""
+    SELECT a.app_id, u_name, j.title, j.company, a.skills, a.experience, a.score
+    FROM applications a 
+    JOIN user u ON a.user_id=u.user_id
+    JOIN jobs j ON a.job_id=j.job_id
+    """)
+
+    for row in cur.fetchall():
+        print(row)
+    conn.close()
 
 def dashboard():
-    pass
+    conn=connect()
+    cur=conn.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM users")
+    u=cur.fetchone()[0]
+
+    cur.execute("SELECT COUNT(*) FROM jobs")
+    j=cur.fetchone()[0]
+
+    cur.execute("SELECT COUNT(*) FROM applications")
+    a=cur.fetchone()[0]
+
+    print("\n--- DASHBOARD ---")
+    print("Users:",u)
+    print("Jobs:",j)
+    print("Applications:",a)
+
+    conn.close()
+
+
 
 def show_mst():
-    pass
-
+    mst,cost=kruskal(edges)
+    print("\nMST:")
+    for u,v,w, in mst:
+        print(u,"-",v,":",w)
+    print("Total Cost: ", cost)
