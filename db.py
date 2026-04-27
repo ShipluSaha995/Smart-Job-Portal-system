b88b26b464d2ed7cc1f6e066bd11cbd7c54eccfb
ShipluSaha995 <shiplusaha995@gmail.com>
2026-04-27 19:10:09 +0600

    updated

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ def connect():
     return mysql.connector.connect(
         host="localhost", 
         user="root",
-        password="!@**Data**1621101##@!",       
+        password="",       
         database="job_portal"
     )
